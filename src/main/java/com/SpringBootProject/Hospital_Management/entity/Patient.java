ef631cfa36a499967f5099b993150077c3bb1de9
dhavalbathe <dhavalbathe@gmail.com>
2026-06-28 15:23:36 +0530

    Basic setup for the project with mysql and two entities are created

---
+++ src/main/java/com/SpringBootProject/Hospital_Management/entity/Patient.java
@@ -0,0 +1,40 @@
+package com.SpringBootProject.Hospital_Management.entity;
+
+import com.SpringBootProject.Hospital_Management.entity.type.BloodGroupType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 40)
+    private String name;
+
+    private LocalDate birthDate;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String gender;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @Enumerated(EnumType.STRING)
+    private BloodGroupType bloodGroup;
+}
---
+++ src/main/java/com/SpringBootProject/Hospital_Management/entity/type/BloodGroupType.java
@@ -0,0 +1,5 @@
+package com.SpringBootProject.Hospital_Management.entity.type;
+
+public class BloodGroupType {
+
+}
